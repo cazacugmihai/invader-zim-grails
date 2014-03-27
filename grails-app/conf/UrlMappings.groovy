0a6b0c87f58ea2d2fe3833e781e5feb74500aa5a
Marco Vermeulen <vermeulen.mp@gmail.com>
2014-03-27 00:12:01 +0000

    Change URL mapping to handle path variable.

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -1,11 +1,7 @@
 class UrlMappings {
 
 	static mappings = {
-		"/$controller/$action?/$id?"{
-			constraints {
-				// apply constraints here
-			}
-		}
+		"/invader/$id"(controller: "invader")
 
 		"/"(view:"/index")
 		"500"(view:'/error')
